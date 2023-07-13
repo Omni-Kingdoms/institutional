e2b897cf02735559ee22c2882d594db26a8b394b
Rafael Ovidio Leal <rafael.leal@stage.consulting>
2023-07-13 16:18:14 -0300

    Initial project

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import Image from "next/image";
+import logo from "@/assets/img/icon-nav.png";
+import Link from "next/link";
+
+export default function Navbar() {
+
+  return (
+    <>
+      <nav className="flex w-full justify-between px-10 py-5 items-center font-extrabold">
+        <Link
+          href="/"
+          className="btn btn-ghost normal-case text-xl  hover:bg-gray-600 "
+        >
+          <Image
+            src={logo}
+            className="   hover:cursor-pointer "
+            alt="chest"
+          />
+        </Link>
+        <div>
+          <ul className="flex px-1 gap-4">
+            <li className="px-3 py-2 rounded hover:bg-gray-600 ">
+            <Link
+                href={
+                  "https://scroll-kingdoms-1.gitbook.io/game-play/game-play"
+                }
+                target="_blank"
+              >
+                Docs
+              </Link>
+            </li>
+
+          </ul>
+        </div>
+        <div>
+          <ul className="flex px-1 gap-4">
+            <li className="px-3 py-3 rounded hover:bg-gray-600">
+              <Link passHref href={"/home"}>
+                <button
+                  className="w-64 px-3 py-2 rounded bg-white text-black"
+                >
+                  Game Dashboard 
+                  {/* <FaArrowRight size={20} color="black"/> */}
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </>
+  );
+}
